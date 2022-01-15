49595f67491576cff139e882667fecc6b8ffa506
Ramon Felix <ramon.felix@zeiss.com>
2022-01-14 21:13:47 -0800

    last changes

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,13 +4,17 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private var score = 0
+    private var _score = 0
+    val score: Int get() = _score
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String get() = _currentScrambledWord
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
+    /**
+     *  Method to obtain the next scramble word
+     */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
@@ -19,17 +23,45 @@ class GameViewModel : ViewModel() {
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 
     init {
         Log.d("GameFragment", "GameViewModel created")
+        getNextWord()
     }
 
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+    * Updates the next word.
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel Destroyed")
     }
-
 }
